f35c85ee6e040e9b2a3916db1eb7d274b9e259a6
Puneet Kashyap <pkashyaps@gmail.com>
2017-10-17 14:35:08 -0400

    updated app bar with menu text

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,5 +1,3 @@
-// @flow weak
-
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
@@ -7,28 +5,78 @@ import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
+import Menu, { MenuItem } from 'material-ui/Menu';
+import Tabs, { Tab } from 'material-ui/Tabs';
+import Hidden from 'material-ui/Hidden';
 
 const style = {
   flex: 1,
 };
 
-const Header = (props) => {
-  // const { classes } = props;
+class Header extends React.Component {
+  state = {
+    anchorEl: null,
+    open: false,
+  };
+
+  handleClick = event => {
+    this.setState({ open: true, anchorEl: event.currentTarget });
+  };
+
+  handleRequestClose = () => {
+    this.setState({ open: false });
+  };
+  render() {
+    const { value } = this.state;
   return (
     <div >
       <AppBar position="static">
         <Toolbar>
-          <IconButton color="contrast" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
+          <img src={require('../../Images/logo1.png')} className="img-responsive" style={{'padding':'10px'}} alt="Andy Nagpal Realtor" />
           <Typography type="title" color="inherit" style={style} >
-            Dilshaan
+            {/* Dilshaan */}
           </Typography>
-          <Button color="contrast">Login</Button>
+          <Hidden only="xs">
+            <Button
+              aria-owns={this.state.open ? 'simple-menu' : null}
+              aria-haspopup="true"
+              color='contrast'
+              onClick={this.handleClick}
+            >
+            <IconButton color='contrast'>
+              <MenuIcon />
+            </IconButton>
+            Menu
+          </Button>
+        </Hidden>
+        <Hidden only="sm">
+          Dilshaan <br/>
+          416-xxx-xxxx
+        </Hidden>
+
+
+          <Menu
+            id="simple-menu"
+            anchorEl={this.state.anchorEl}
+            open={this.state.open}
+            onRequestClose={this.handleRequestClose}
+          >
+            <MenuItem onClick={this.handleRequestClose}>Home</MenuItem>
+            <MenuItem onClick={this.handleRequestClose}>Albums</MenuItem>
+            <MenuItem onClick={this.handleRequestClose}>Contact Me</MenuItem>
+        </Menu>
         </Toolbar>
+          <Tabs centered scrollable value={value} onChange={this.handleChange}>
+              <Tab label="Home" />
+              <Tab label="Shows" />
+              <Tab label="Albums" />
+              <Tab label="Schedules" />
+              <Tab label="Bookings" />
+              <Tab label="Contact Me" href="#basic-tabs" />
+          </Tabs>
       </AppBar>
     </div>
-  );
+  )};
 }
 
 export default Header;
