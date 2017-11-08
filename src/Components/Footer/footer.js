548a984678eee468397177d231c18be330277523
Puneet Kashyap <pkashyaps@gmail.com>
2017-11-08 16:06:33 -0500

    telephone href added to contact and footer

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -5,23 +5,27 @@ import Typography from 'material-ui/Typography';
 import Grid from 'material-ui/Grid'
 import { connect } from 'react-redux';
 
+import './footer.css';
+
 const Footer = (props) => {
   return (
     <div >
       <AppBar position="static">
         <Toolbar>
-          <Grid container justify="space-around" alignItems="center" spacing={24} style={{ margin: 5 }}>
+          <Grid container justify="space-around" alignItems="center" spacing={24} style={{ margin: '5px' }}>
 
             <Grid item xs={12} sm={4}>
               <Typography align='center' type="display1"  color="inherit" style={{ 'padding': '8px' }}>
                 {props.owner.owner1.name}
               </Typography>
-              <Typography align='center' type="headline"  color="inherit" style={{ color: 'bisque'}}>
-                  {props.owner.owner1.phone}
+              <Typography align='center' type="headline"  color="inherit">
+                  <a href="tel:+917696571231" className="hyper-link">
+                    {props.owner.owner1.phone}
+                  </a>
               </Typography>
               <Typography align='center' type="headline"  color="inherit" >
                   <a href={`mailto:${props.owner.owner1.name}?Subject=Dilshaan`}
-                    style={{ color: 'bisque'}} target="_top">
+                    className="hyper-link" target="_top">
                     {props.owner.owner1.email}
                   </a>
               </Typography>
@@ -40,17 +44,19 @@ const Footer = (props) => {
               <Typography align='center' type="display1"  color="inherit" style={{ 'padding': '8px' }}>
                 {props.owner.owner2.name}
               </Typography>
-              <Typography align='center' type="headline"  color="inherit" style={{ color: 'bisque'}}>
+              <Typography align='center' type="headline"  color="inherit">
+                <a href="tel:+919814581786" className="hyper-link">
                   {props.owner.owner2.phone}
+                </a>
               </Typography>
               <Typography align='center' type="headline"  color="inherit" >
                   <a href={`mailto:${props.owner.owner2.email}?Subject=Ramesh%20Sippy`}
-                    style={{ color: 'bisque'}} target="_top">
+                    className="hyper-link" target="_top">
                     {props.owner.owner2.email}
                   </a>
               </Typography>
             </Grid>
-            
+
           </Grid>
         </Toolbar>
       </AppBar>
