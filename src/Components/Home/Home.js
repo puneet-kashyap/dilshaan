8c800bcf8cb0d9ece969e5e59bb55638d369642b
Puneet Kashyap <pkashyaps@gmail.com>
2017-11-18 19:47:54 -0500

    updated biography paragraphs

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -18,17 +18,21 @@ const Home = () => {
             Dilshaan
           </Typography>
           <Typography paragraph type="headline" style={{'padding':'0px 20px'}}>
-            My real name is Rajat Kumar, s/o Ramesh Kumar Sippy.
-            I am residing at Punjabi Bagh Kapurthalia, Punjab India.
-            Music fascinated me from birth. Since the age of 3 to 4 years, I was very fond of Tabla and Harmonium.
-            Started singing at the age of 8 to 10 years from the most reputed and classical singer Mr. Daler Ali ji, who is settled in Jalandhar. Still I am taking music education from guru ji.
+            Music fascinated me from birth.
+            Since the age of 3, I was very fond of Tabla and Harmonium.
+            Under the guidance of most reputed and classical singer Mr. Daler Ali ji, I have been singing from the age of 8 years.
           </Typography>
-          <Typography paragraph type="headline" component='p' style={{'padding':'0px 20px'}}>
+          <Typography paragraph type="headline" style={{'padding':'0px 20px'}}>
+            Music is in our genes as my father Ramesh K.
+            Sippy is also in Punjabi music industry as a well known lyricist who always encourage me to continue my passion of singing.
+            My father and brother Rajan Kumar, are the big force behind me to convert my passion into profession.
+          </Typography>
+          <Typography paragraph type="headline" style={{'padding':'0px 20px'}}>
             I have done bachelor's & masters in Music, both vocal and instrumental.
-            I have also participated in youth festival and other music competition in various schools and college, with 1st position and 2nd position in singing.
-            First romantic Punjabi song audio and video released on 1st October, 2017 through Brown Eyes music entertainer.
-            Check out the full video on Youtube.
-            Presently running a music Academy. ( Talent Academy)
+            I also took part in many competition such as school competition, inter school competition, and many youth festivals in which I got huge appreciation along with many prizes.
+          </Typography>
+          <Typography paragraph type="headline" style={{'padding':'0px 20px'}}>
+            With the blessings of my Guru ji and support of my family & friends, my first romantic punjabi video song was released in October 2017 through Brown Eyes music entertainer.
           </Typography>
       </div>
       <div className="row col-md-offset-1 col-md-10" >
